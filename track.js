ab3bfbfc2850e7cbeeeaccfa18a468ebcff1929e
Necdet Can Ateşman <can.atesman@strg.at>
2016-11-11 15:45:34 +0100

    Fix: Multiple issues in track.js

--- track.js
+++ track.js
@@ -16,12 +16,13 @@
     var clientId = localStorage.getItem('strg.metrics.client');
     if (!clientId) {
         clientId = generateRandomID();
+        localStorage.setItem('strg.metrics.client', clientId);
         localStorage.setItem('strg.metrics.client.state', '{}');
     } else if (!localStorage.getItem('strg.metrics.client.state')) {
         localStorage.setItem('strg.metrics.client.state', '{}');
     }
 
-    var sessionId = sessionStorage.getItem('strg.metrics.client');
+    var sessionId = sessionStorage.getItem('strg.metrics.session');
     if (!sessionId) {
         sessionId = generateRandomID();
         sessionStorage.setItem('strg.metrics.session', sessionId);
@@ -40,9 +41,14 @@
         if (!queue.length) {
             return;
         }
-        queue.splice(0, 0, clientId, windowId);
-        connection.send(JSON.stringify(queue));
+        if (!connection || connection.readyState != window.WebSocket.OPEN) {
+            return;
+        }
+        var payload = queue;
         queue = [];
+        payload.splice(0, 0, clientId, windowId);
+        console.log('Sending', payload);
+        connection.send(JSON.stringify(payload));
     };
 
     var connection = new window.WebSocket('ws://localhost:8081');
@@ -50,6 +56,10 @@
     connection.onopen = function() {
         flush();
     };
+
+    window.addEventListener('beforeunload', function() {
+        connection.close();
+    });
 
     var flushTimeout = null;
 
@@ -66,7 +76,6 @@
 
         clientStateChange: function clientStateChange(key, value) {
             clientState = JSON.parse(localStorage.getItem('strg.metrics.client.state'));
-            console.log(clientState, key, value);
             if (clientState[key] === value) {
                 return;
             }
