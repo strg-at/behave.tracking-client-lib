e126202ad4d19d8078f13032d39206069cf1dc57
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2018-08-14 16:51:44 +0200

    Change: Major rewrite of tracker
      - Use objects as message format
      - Send only one event per frame
      - Use global namespace var 'strgMetricsId'
      - Remove obsolete code
      - Minor bugfixes and code-formatting
    
    INF-15

--- track.js
+++ track.js
@@ -1,253 +1,224 @@
 /**
  * @module strg/metrics
  */
-(function(window) {
+(function (window) {
 
-    /* eslint-env browser*/
-    /* global screen, localStorage, sessionStorage, navigator */
+  /* eslint-env browser*/
+  /* global screen, localStorage, sessionStorage, navigator */
 
-    var generateRandomID = function generateRandomID() {
-        return Math.floor(Math.random() * Math.pow(36, 5)).toString(36) +
-            Math.floor(Math.random() * Math.pow(36, 5)).toString(36) +
-            Math.floor(Math.random() * Math.pow(36, 5)).toString(36) +
-            Math.floor(Math.random() * Math.pow(36, 5)).toString(36);
-    };
-
-    var clientId = localStorage.getItem('strg.metrics.client');
-    if (!clientId) {
-        clientId = generateRandomID();
-        localStorage.setItem('strg.metrics.client', clientId);
-        localStorage.setItem('strg.metrics.client.state', '{}');
-    } else if (!localStorage.getItem('strg.metrics.client.state')) {
-        localStorage.setItem('strg.metrics.client.state', '{}');
-    }
-
-    var sessionId = sessionStorage.getItem('strg.metrics.session');
-    if (!sessionId) {
-        sessionId = generateRandomID();
-        sessionStorage.setItem('strg.metrics.session', sessionId);
-    }
-
-    var windowId = generateRandomID();
-    sessionStorage.setItem('strg.metrics.window', windowId);
-
-    var windowState = {};
-
-    var queue = [];
-
-    var clientState;
-
-    var flush = function() {
-        flushTimeout = null;
-        if (!queue.length) {
-            return;
-        }
-        if (!connection || connection.readyState != window.WebSocket.OPEN) {
-            // this function wil be called again once the connection is open
-            return;
-        }
-        connection.send(JSON.stringify(queue));
-        queue = [];
-        window.clearTimeout(keepAliveTimeout);
-        queueKeepalive();
-    };
-
-    function queueKeepalive() {
-        keepAliveTimeout = window.setTimeout(function() {
-            connection.send(JSON.stringify([]));
-            queueKeepalive();
-        }, 10000);
-    }
-
-    var endpoint;
-
-    var connection = null;
-
-    var wasConnected = false;
-
-    var keepAliveTimeout = null;
-
-    function connect() {
-
-        if (connection) {
-            if (console && typeof console.warn == 'function') {  // eslint-disable-line no-console
-                console.warn('Duplicate metrics connect');       // eslint-disable-line no-console
-            }
-            enqueue('metrics.error', 'Duplicate metrics connect');
-            return;
-        }
-
-        connection = new window.WebSocket(endpoint);
-
-        connection.onopen = function() {
-            connection.send(JSON.stringify([new Date().getTime() - startTime, clientId, windowId, wasConnected]));
-            wasConnected = true;
-            flush();
-        };
-
-        connection.onclose = function() {
-            if (!shuttingDown) {
-                connection = null;
-                window.setTimeout(connect, 500);
-            }
-        };
-
-    }
-
-    var shuttingDown = false;
-
-    window.addEventListener('beforeunload', function() {
-        shuttingDown = true;
-        flush();
-        if (connection) {
-            connection.close();
-        }
+  /**
+   * uuid4 implementation taken from:
+   * https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+   * also see https://bocoup.com/blog/random-numbers
+   */
+  function uuid4() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (dt + Math.random() * 16) % 16 | 0;
+      dt = Math.floor(dt / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
+    return uuid;
+  }
 
-    var flushTimeout = null;
+  /**
+   * The global object would already be declared by an inline snippet.
+   * Similar to GA we stores a identifier in an other global: `strgMetricsId`.
+   * Defaults to 'strg'.
+   */
+  var GLOBAL_NAME = window.strgMetricsId || 'strg';
+  window[GLOBAL_NAME] = window[GLOBAL_NAME] || {};
 
-    var enqueue = function enqueue(key, value) {
-        queue.push([new Date().getTime() - startTime, key, value]);
-        if (!flushTimeout) {
-            flushTimeout = window.setTimeout(flush, 500);
-        }
+  /**
+   * Prepare life-cycle hashes for client, session and window.
+   *   - `clientHash` is bound to `window.localStorage`.
+   *   - `sessionHash` is bound to `window.sessionStorage`.
+   *   - `windowHash` is bound to the current JS scope `window`.
+   */
+  var clientHash = localStorage.getItem('strg.metrics.client');
+  if (!clientHash) {
+    clientHash = uuid4();
+    localStorage.setItem('strg.metrics.client', clientHash);
+    localStorage.setItem('strg.metrics.client.state', '{}');
+  } else if (!localStorage.getItem('strg.metrics.client.state')) {
+    localStorage.setItem('strg.metrics.client.state', '{}');
+  }
+
+  var sessionHash = sessionStorage.getItem('strg.metrics.session');
+  if (!sessionHash) {
+    sessionHash = uuid4();
+    sessionStorage.setItem('strg.metrics.session', sessionHash);
+  }
+
+  var windowHash = window[GLOBAL_NAME].window = window[GLOBAL_NAME].window || uuid4();
+  var windowState = {};
+  var queue = [];
+  var clientState;
+
+  /**
+   * Flush events in queue as single frames.
+   */
+  function flush () {
+    flushTimeout = null;
+    if (!connection || connection.readyState != window.WebSocket.OPEN) {
+      // this function will be called again once the connection is open
+      return;
+    }
+    while (queue.length) {
+      connection.send(JSON.stringify(queue[0]));
+      queue.shift();
+    }
+  };
+
+  var endpoint;
+  var connection = null;
+  var shuttingDown = false;
+
+  /**
+   * Open connection to the WebSocket, handle reconnects and first frame.
+   */
+  function connect() {
+    connection = new window.WebSocket(endpoint);
+
+    /**
+     * On connection open always send the first frame with the three hashes.
+     */
+    connection.onopen = function () {
+      connection.send(JSON.stringify({
+        delta: 1 * new Date() - startTime,
+        client: clientHash,
+        session: sessionHash,
+        window: windowHash
+      }));
+      flush();
     };
 
-    var initialized = false;
-
-    var startTime = new Date().getTime();
-
-    var tracker = {
-
-        /**
-         * Initialize metrics.
-         *
-         * This is the first thing, that needs to be called when using metrics.
-         * It should also be called at most once!
-         *
-         * The optional *endpointUrl* to the websocket server may start with a
-         * protocol description ("wss://..."). If it does not, the protocol
-         * will be prepended automatically, depending on the current connection
-         * security. If we are on an "https" domain, the protocol will be
-         * "wss://", otherwise it will be "ws://".
-         *
-         * If the value for *endpointUrl* is omitted, it defaults to the
-         * current domain, but on port 8081, with an auto-detected protocol as
-         * described above.
-         */
-        init: function init(endpointUrl) {
-            if (initialized) {
-                if (console && typeof console.warn == 'function') {            // eslint-disable-line no-console
-                    console.warn('Duplicate initialization of strg.metrics');  // eslint-disable-line no-console
-                }
-                enqueue('metrics.error', 'Duplicate metrics init');
-                return;
-            }
-            initialized = true;
-            if (endpointUrl) {
-                endpoint = endpointUrl;
-            } else {
-                var hostname = window.location.hostname ? window.location.hostname : 'localhost';
-                endpoint = hostname + ':8081';
-            }
-            if (endpoint.indexOf('://') === -1) {
-                var protocol = window.location.protocol == 'https:' ? 'wss' : 'ws';
-                endpoint = protocol + '://' + endpoint;
-            }
-            connect();
-        },
-
-        /**
-         * Unconditionally triggers an event with given key and value.
-         */
-        trigger: function trigger(key, value) {
-            enqueue(key, value);
-        },
-
-        /**
-         * Notifies metrics of a state change within the client.
-         *
-         * This function is similar to :js:func:`trigger`, but will only
-         * trigger the event, if the value is different than the value of the
-         * last call.
-         */
-        clientStateChange: function clientStateChange(key, value) {
-            clientState = JSON.parse(localStorage.getItem('strg.metrics.client.state'));
-            if (clientState[key] === value) {
-                return;
-            }
-            clientState[key] = value;
-            localStorage.setItem('strg.metrics.client.state', JSON.stringify(clientState));
-            enqueue(key, value);
-        },
-
-        /**
-         * Notifies metrics of a state change within the window.
-         *
-         * This function is similar to :js:func:`clientStateChange`, but has a
-         * different scope: It will "forget" all previous values on each page
-         * load.
-         */
-        windowStateChange: function windowStateChange(key, value) {
-            if (windowState[key] === value) {
-                return;
-            }
-            windowState[key] = value;
-            enqueue(key, value);
-        },
-
-        /**
-         * Sets the initial window state without triggering an event.
-         *
-         * This function is automatically used for storing the current location
-         * hash, for example. The function will the track changes to the
-         * location hash and call :js:func:`windowStateChange` once it changes.
-         * That function will the trigger the event, but only if it differs
-         * from its previous value (which was set here for the first time).
-         */
-        windowStateInit: function windowStateInit(key, value) {
-            windowState[key] = value;
-        }
-
+    /**
+     * Try to reconnect on close.
+     */
+    connection.onclose = function () {
+      if (!shuttingDown) {
+        connection = null;
+        window.setTimeout(connect, 500);
+      }
     };
+  }
 
-    /* ClientState */
-
-    tracker.clientStateChange('session', sessionId);
-
-    tracker.clientStateChange('screen.resolution', screen.width + "x" + screen.height);
-
-    tracker.clientStateChange('screen.colordepth', screen.colorDepth);
-
-    if (screen.orientation && screen.orientation.type) {
-        tracker.clientStateChange('screen.orientation', screen.orientation.type);
+  /**
+   * Try to flush remaining events on 'beforeunload'.
+   */
+  window.addEventListener('beforeunload', function () {
+    shuttingDown = true;
+    flush();
+    if (connection) {
+      connection.close();
     }
+  });
 
-    tracker.clientStateChange('navigator.user_agent', navigator.userAgent);
-
-    tracker.clientStateChange('navigator.language', navigator.language);
-
-    if (navigator.systemLanguage) {
-        tracker.clientStateChange('navigator.system_language', navigator.systemLanguage);
-    }
-
-    /* WindowState */
-
-    tracker.windowStateChange('url', window.location.href);
-
-    if (document.referrer) {
-        tracker.windowStateChange('referrer', document.referrer);
-    }
-
-    if (window.location.hash) {
-        tracker.windowStateInit('url.hash', window.location.hash);
-    }
-
-    window.addEventListener("hashchange", function() {
-        tracker.windowStateChange('url.hash', window.location.hash);
+  /**
+   * Adds events to the queue and sets the time delta for when the event was triggered.
+   * @param {string} key - The event name, e.g. 'article.id'.
+   * @param {string|number} value - The events value, e.g. 'Art-123456'
+   */
+  function enqueue(key, value) {
+    queue.push({
+      delta: 1 * new Date() - startTime,
+      key: key,
+      value: value
     });
+    flush();  // Immediate flush with no timeout
+  };
 
-    window.strg = window.strg || {};
-    window.strg.metrics = tracker;
+  var startTime = window[GLOBAL_NAME].t || 1 * new Date();  // Try to fetch from global
+
+  var tracker = {
+    /**
+     * Initialize metrics.
+     * @param {string} url - *endpoint URL* for the websocket server.
+     *
+     * This is the first thing, that needs to be called when using metrics.
+     * It should also be called at most once!
+     *
+     * The *url* to the websocket server may start with aprotocol description
+     * ("wss://..."). If it does not, the protocol will be prepended
+     * automatically, depending on the current connection security.
+     * If we are on an "https" domain, the protocol will be "wss://",
+     * otherwise it will be "ws://".
+     */
+    init: function init(url) {
+      if (!url) {
+        throw Error('Configuration Error: Missing endpoint url');
+      }
+      endpoint = url;
+      if (endpoint.indexOf('://') === -1) {
+        var protocol = window.location.protocol == 'https:' ? 'wss' : 'ws';
+        endpoint = protocol + '://' + endpoint;
+      }
+      connect();
+    },
+
+    /**
+     * Unconditionally triggers an event with given key and value.
+     * @param {string} key - The event name, e.g. 'article.id'.
+     * @param {string|number} value - The events value, e.g. 'Art-123456'
+     */
+    trigger: function trigger(key, value) {
+      enqueue(key, value);
+    },
+
+    /**
+     * Notifies metrics of a state change within the client.
+     * @param {string} key - The event name.
+     * @param {string|number} value - The event value.
+     *
+     * This function is similar to :js:func:`trigger`, but will only
+     * trigger the event, if the value is different than the value of the
+     * last call.
+     */
+    clientStateChange: function clientStateChange(key, value) {
+      clientState = JSON.parse(localStorage.getItem('strg.metrics.client.state'));
+      if (clientState[key] === value) {
+        return;
+      }
+      clientState[key] = value;
+      localStorage.setItem('strg.metrics.client.state', JSON.stringify(clientState));
+      enqueue(key, value);
+    },
+
+    /**
+     * Notifies metrics of a state change within the window.
+     * @param {string} key - The event name.
+     * @param {string|number} value - The event value.
+     *
+     * This function is similar to :js:func:`clientStateChange`, but has a
+     * different scope: It will "forget" all previous values on each page
+     * load.
+     */
+    windowStateChange: function windowStateChange(key, value) {
+      if (windowState[key] === value) {
+        return;
+      }
+      windowState[key] = value;
+      enqueue(key, value);
+    },
+
+    /**
+     * Sets the initial window state without triggering an event.
+     * @param {string} key - The event name.
+     * @param {string|number} value - The event value.
+     *
+     * So calling :js:func:`windowStateChange` will only trigger the event
+     * if it differs from the value previously set with this function.
+     */
+    windowStateInit: function windowStateInit(key, value) {
+      windowState[key] = value;
+    }
+
+  };
+
+  /**
+   * Finally assign the tracker to the global variable
+   */
+  window[GLOBAL_NAME].metrics = tracker;
 
 })(this);
