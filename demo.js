dcf870980bd249ea3d6230988d69c48028a0c9a3
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2018-11-06 16:35:52 +0100

    New: Add demo
    BHV-12

---
+++ demo.js
@@ -0,0 +1,38 @@
+/* eslint-env browser */
+(function () {
+
+  var tracker = window.strg.metrics;
+
+  window.strg.logger = console;
+
+  tracker.init('ws://35.204.167.129:3000');
+  // tracker.visibility.init();
+
+  // /* ClientState */
+  tracker.clientStateChange('screen.resolution', screen.width + "x" + screen.height);
+  screen.orientation && screen.orientation.type &&
+    tracker.clientStateChange('screen.orientation', screen.orientation.type);
+  tracker.clientStateChange('navigator.user_agent', navigator.userAgent);
+  // tracker.clientStateChange('navigator.language', navigator.language);
+  // navigator.systemLanguage &&
+  //   tracker.clientStateChange('navigator.system_language', navigator.systemLanguage);
+
+  // /* WindowState */
+  tracker.windowStateChange('url', window.location.href);
+  document.referrer &&
+    tracker.windowStateChange('referrer', document.referrer);
+  window.location.hash &&
+    tracker.windowStateInit('url.hash', window.location.hash);
+  window.addEventListener("hashchange", function () {
+    tracker.windowStateChange('url.hash', window.location.hash);
+  });
+
+  // var articleId = parseArticleId(location.href);
+  // articleId && window.strg.metrics.windowStateChange('article.id', articleId);
+
+  // var articleSelector = 'article[id="article-' + articleId +'"]';
+  // if (document.querySelector(articleSelector)) {
+  //   window.strg.metrics.breakpointMeter.percent(articleSelector, 'article');
+  // }
+
+})();
