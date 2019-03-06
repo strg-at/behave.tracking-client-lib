0548cbebf0ac04f780769a2126ca1d9e82b48a8c
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-03-06 16:23:02 +0100

    Change: Move visibility and breakpoint tracking to plugins, BHV-93

---
+++ src/plugins/plugin.visibility.js
@@ -0,0 +1,29 @@
+/**
+ * @module strg/metrics/visibility
+ */
+export function createVisibilityTracking (global, { tracker }) {
+  let hidden
+  let visibilityChange
+
+  if (typeof global.document.hidden !== 'undefined') {
+    hidden = 'hidden'
+    visibilityChange = 'visibilitychange'
+  } else if (typeof global.document.msHidden !== 'undefined') {
+    hidden = 'msHidden'
+    visibilityChange = 'msvisibilitychange'
+  } else if (typeof global.document.webkitHidden !== 'undefined') {
+    hidden = 'webkitHidden'
+    visibilityChange = 'webkitvisibilitychange'
+  }
+
+  return {
+    init () {
+      if (visibilityChange) {
+        tracker.windowStateChange('window.active', !global.document[hidden])
+        global.document.addEventListener(visibilityChange, function () {
+          tracker.windowStateChange('window.active', !global.document[hidden])
+        }, false)
+      }
+    }
+  }
+}
