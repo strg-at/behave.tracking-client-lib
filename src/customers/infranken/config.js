2d3e6e40bed858085111fcd722ec614c02181c63
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-03-07 16:06:17 +0100

    Change: Enable recommendations for infranken

--- src/customers/infranken/config.js
+++ src/customers/infranken/config.js
@@ -11,5 +11,5 @@ export default {
   RECOMMENDATION_APP_URL: '//behave.infranken.de/static/app.3.0.0.js',
   TRACKING_SERVICE_URL: process.env.NODE_ENV === 'development'
     ? 'ws://localhost:8008'
-    : 'wss://behave.infranken.de/tracking-service'
+    : 'wss://behave.infranken.de/ws/event'
 }
