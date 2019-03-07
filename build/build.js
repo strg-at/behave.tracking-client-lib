e5ad945f9f42db65861b57918559e05166a9693e
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-03-07 14:41:44 +0100

    New: Customer build options for webpack BHV-89

--- build/build.js
+++ build/build.js
@@ -1,32 +1,45 @@
 // eslint-disable-next-line node/no-extraneous-require
 require('dotenv').config()
 const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 
-const outputPath = process.env.OUTPUT_PATH
+const CUSTOMER = process.env.CUSTOMER
+
+CUSTOMER && console.log(`Building for CUSTOMER: ${CUSTOMER}`)
+
+const OUTPUT_PATH = process.env.OUTPUT_PATH
   ? process.env.OUTPUT_PATH
   : '../dist/'
 const watch = !!process.env.WATCH
 
 webpack({
   // Configuration Object
-  mode: 'production',
+  // mode: 'production',
+  mode: process.env.NODE_ENV || 'production',
   devtool: process.env.NODE_ENV === 'development'
     ? 'inline-source-map'
     : false,
   watch,
   // context: path.resolve(__dirname, '../src/'),
-  entry: './src/index.js',
+  entry: CUSTOMER
+    ? `./src/customers/${CUSTOMER}/index.js`
+    : './src/index.js',
   output: {
-    path: path.resolve(__dirname, outputPath),
-    filename: 'track.js'
+    path: path.resolve(__dirname, OUTPUT_PATH),
+    publicPath: process.env.NODE_ENV === 'development'
+      ? '/dist/'
+      : '/',
+    filename: 'init.js',
+    chunkFilename: '[name].[chunkhash].js',
   },
   plugins: [
     /**
      * Renders process.env.VARIABLE to strings in build
      */
-    new Dotenv()
+    new Dotenv(),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
@@ -37,6 +50,9 @@ webpack({
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            /**
+             * Note: transform-runtime is only useful when we have a lot of code splitting
+             */
             plugins: ['@babel/plugin-transform-runtime']
           }
         }
