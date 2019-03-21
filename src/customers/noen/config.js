a81a03d3ea3f22144ea7d1df0f8993511596c221
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-03-21 08:46:01 +0100

    Change: Update cookie name, BHV-131

--- src/customers/noen/config.js
+++ src/customers/noen/config.js
@@ -3,7 +3,7 @@
  */
 export default {
   NAMESPACE: 'strgBeHave',
-  COOKIE_NAME: 'BeHaveOptout',
+  COOKIE_NAME: 'STRG.BeHaveOptOut',
   APP_NODE_IDS: [
   ],
   RECOMMENDATION_APP_URL: 'https://behave.noen.at/api/recommendation',
