55135b00cee84a0ed5ba8d3a6e08d47a49b46378
Nils Müller <nils.mueller@strg.at>
2019-04-30 17:05:10 +0200

    New: added customer chip, npm audit fix BHV-155

---
+++ src/customers/chip/config.js
@@ -0,0 +1,11 @@
+/**
+ * Sn Configuration
+ */
+export default {
+  NAMESPACE: 'strgBeHave',
+  COOKIE_NAME: 'STRG.BeHaveOptOut',
+  APP_NODE_IDS: [
+  ],
+  RECOMMENDATION_APP_URL: 'https://behave.chip.de/api/recommendation',
+  TRACKING_SERVICE_URL: 'wss://behave.chip.de/ws/event',
+}
