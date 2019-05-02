7df4cda08fd10a9ad6ad32ce5bb3214be9c88e07
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-05-02 15:03:21 +0200

    New: Add standard tracker implementation, BHV-166

---
+++ src/tracker/standard.js
@@ -0,0 +1,43 @@
+/**
+ * @module StandardTracker
+ * Standard Tracker DOM-implementation
+ */
+
+import { createTracker } from './tracker'
+import { createScrollTracking } from '../plugins/plugin.scroll'
+import { createVisibilityTracking } from '../plugins/plugin.visibility'
+
+export function configureTracker (global, config) {
+  const { endpoint, articleSelector } = config
+
+  /**
+   * Create the tracker
+  */
+  const tracker = createTracker(global, config)
+
+  tracker.use('scrollTracking', createScrollTracking(global, { tracker }))
+  tracker.use('visibilityTracker', createVisibilityTracking(global, { tracker }))
+
+  tracker.init(endpoint)
+  tracker.visibilityTracker.init()
+
+  /* WindowState */
+  tracker.windowStateChange('url', global.location.href)
+  document.referrer &&
+  tracker.windowStateChange('referrer', document.referrer)
+  global.location.hash &&
+  tracker.windowStateInit('url.hash', global.location.hash)
+  global.addEventListener('hashchange', function () {
+    tracker.windowStateChange('url.hash', global.location.hash)
+  })
+
+  if (document.querySelector(articleSelector)) {
+    tracker.scrollTracking.scrollDepth(
+      articleSelector,
+      {
+        eventKey: 'breakpoint.content.percent.max',
+      })
+  }
+
+  return tracker
+}
