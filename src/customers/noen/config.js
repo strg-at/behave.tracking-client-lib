612a0e4e13a8e5358c75035bf06b45df3d30cb23
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-06-27 22:48:21 +0200

    Fix: Add proper app url, BHV-280

--- src/customers/noen/config.js
+++ src/customers/noen/config.js
@@ -4,10 +4,8 @@
 module.exports = {
   NAMESPACE: 'strgBeHave',
   COOKIE_NAME: 'STRG.BeHaveOptOut',
-  APP_NODE_IDS: [
-  ],
   PUBLIC_PATH: '//behave.noen.at/static/',
-  RECOMMENDATION_APP_URL: 'https://behave.noen.at/api/recommendation',
+  RECOMMENDATION_APP_URL: 'https://behave.noen.at/static/app.0.1.0.js',
   RECOMMENDATION_APP_CUSTOM_ELEMENTS: [
     'behave-recommendation-box',
   ],
--- src/customers/sn/config.js
+++ src/customers/sn/config.js
@@ -5,7 +5,7 @@ module.exports = {
   NAMESPACE: 'strgBeHave',
   COOKIE_NAME: 'STRG.BeHaveOptOut',
   PUBLIC_PATH: '//behave.sn.at/static/',
-  RECOMMENDATION_APP_URL: 'https://behave.sn.at/api/recommendation',
+  RECOMMENDATION_APP_URL: 'https://behave.sn.at/static/app.0.1.0.js',
   RECOMMENDATION_APP_CUSTOM_ELEMENTS: [],
   TRACKING_SERVICE_URL: 'wss://behave.sn.at/ws/event',
   ARTICLE_SELECTOR: 'article.article',
