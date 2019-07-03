50175e5c83e595aebae606d6f3ef0e79a98561e9
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-07-03 17:22:59 +0200

    Change: Enable app in sn, BHV-251

--- src/customers/sn/app.js
+++ src/customers/sn/app.js
@@ -3,23 +3,23 @@
  * Load and init the recommendation app as external script
  */
 
-// import { loadScript } from '../../utils/utils'
+import { loadScript } from '../../utils/utils'
 
 export function loadRecommandationsApp (global, config) {
-  // const {
-  //   RECOMMENDATION_APP_URL,
-  //   RECOMMENDATION_APP_CUSTOM_ELEMENTS,
-  // } = config
+  const {
+    RECOMMENDATION_APP_URL,
+    RECOMMENDATION_APP_CUSTOM_ELEMENTS,
+  } = config
 
-  // /**
-  //  * Test if custom elements were not previously created and still have
-  //  * HTMLElement as constructor
-  //  */
-  // const doCreateElement = RECOMMENDATION_APP_CUSTOM_ELEMENTS.filter(element => {
-  //   return document.createElement(element).constructor === HTMLElement
-  // }).length > 0
+  /**
+   * Test if custom elements were not previously created and still have
+   * HTMLElement as constructor
+   */
+  const doCreateElement = RECOMMENDATION_APP_CUSTOM_ELEMENTS.filter(element => {
+    return document.createElement(element).constructor === HTMLElement
+  }).length > 0
 
-  // if (doCreateElement) {
-  //   loadScript(global, RECOMMENDATION_APP_URL)
-  // }
+  if (doCreateElement) {
+    loadScript(global, RECOMMENDATION_APP_URL)
+  }
 }
--- src/customers/sn/config.js
+++ src/customers/sn/config.js
@@ -5,8 +5,10 @@ module.exports = {
   NAMESPACE: 'strgBeHave',
   COOKIE_NAME: 'STRG.BeHaveOptOut',
   PUBLIC_PATH: '//behave.sn.at/static/',
-  RECOMMENDATION_APP_URL: 'https://behave.sn.at/static/app.0.1.0.js',
-  RECOMMENDATION_APP_CUSTOM_ELEMENTS: [],
+  RECOMMENDATION_APP_URL: 'https://behave.sn.at/static/app.0.0.1.js',
+  RECOMMENDATION_APP_CUSTOM_ELEMENTS: [
+    'behave-recommendation-box',
+  ],
   TRACKING_SERVICE_URL: 'wss://behave.sn.at/ws/event',
   ARTICLE_SELECTOR: 'article.article',
 }
