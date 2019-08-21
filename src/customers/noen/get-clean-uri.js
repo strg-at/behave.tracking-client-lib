b1015c648662218cf898e3de6736c844336783be
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-08-21 16:58:24 +0200

    New: Replace mobile prefix in noen, BHV-367

--- src/customers/noen/get-clean-uri.js
+++ src/customers/noen/get-clean-uri.js
@@ -1,10 +1,12 @@
 /**
  * Parse clean url without hashes and query params from location object
+ * Replace mobile prefix `m.` with canonical `www.`
  * @param {Location} location - window.location object
  */
 export function getCleanURI (location) {
+  const hostname = location.hostname.replace(/^m\./, 'www.')
   return location.protocol +
     '//' +
-    location.hostname +
+    hostname +
     (location.port ? ':' + location.port : '') + location.pathname
 }
