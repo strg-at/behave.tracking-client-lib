e1b524b054c950d7d3c6f9cf74f3f7ef6ca49ab2
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-08-22 10:42:45 +0200

    Change: Update noen app to 0.3.0, BHV-370

--- src/customers/noen/config.js
+++ src/customers/noen/config.js
@@ -7,7 +7,7 @@ module.exports = {
   NAMESPACE: 'strgBeHave',
   COOKIE_NAME: 'STRG.BeHaveOptOut',
   PUBLIC_PATH: '//behave.noen.at/static/',
-  RECOMMENDATION_APP_URL: 'https://behave.noen.at/static/app.0.2.0.js',
+  RECOMMENDATION_APP_URL: 'https://behave.noen.at/static/app.0.3.0.js',
   RECOMMENDATION_APP_CUSTOM_ELEMENTS: [
     'behave-recommendation-box',
     'behave-noen-startpage-box',
