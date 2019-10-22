82af8a59c1177cc5dfbf2f91f4f4deef32522d9c
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2019-10-22 14:11:38 +0200

    Doc: Change wording for test stub, BHV-465

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 /* eslint-env jest */
 import { createTracker } from './index.js'
 
-test('Can import lib and call createTracker', () => {
+test('Can import createTracker as function', () => {
   expect(typeof createTracker).toBe('function')
 })
 
-test('One more test', () => {
+test('Result of createTracker has init function', () => {
   const tracker = createTracker(global, { NAMESPACE: 'strgBeHave' })
   expect(typeof tracker.init).toBe('function')
 })
