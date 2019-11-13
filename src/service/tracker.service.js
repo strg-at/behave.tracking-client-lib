05e0f97be4aff5edccc61f961b4c3c7bf2eba1d5
Nils Müller <nils.mueller@strg.at>
2019-11-13 13:20:10 +0100

    Change: updated service layer, removed not needed dataLayer functionality BHV-466

--- src/service/tracker.service.js
+++ src/service/tracker.service.js
@@ -1,26 +1,42 @@
 export class TrackerService {
   constructor (dao, conf) {
     this.dao = dao
-    this.dataLayer = []
   }
 
+  /**
+   * add all events in events
+   * validate events before sending them to dao layer
+   * expect events to be an array of well formed event items
+   * @param {Event[]} events - an Array of Event items
+   * @returns {Number} the number of items properly proceeded
+   */
   addEvents (events) {
     console.log(events)
+    let count = 0
     if (!Array.isArray(events)) {
-      return
+      return count
     }
     events.forEach(event => {
-      this.validateEvent(event)
-      this.dataLayer.push(event)
+      try {
+        this.validateEvent(event)
+      } catch (err) {
+        console.error(err)
+        return
+      }
+      count++
+      this.dao.store(event)
     })
-    // TODO trigger proceed dataLayer items
-    console.log(this.dataLayer)
-    return this.dataLayer.length
+    return count
   }
 
+  /**
+   * validate an Event
+   * @throws Error when input is not an object or null
+   * @throws Error when event.type is not present or invalid
+   * @param {Event} event - the Event to validate
+   */
   validateEvent (event) {
     // TODO: check if type, key and value are set properly
-    console.log(typeof event)
     if (typeof event !== 'object' || event === null) {
       throw new Error('empty or invalid event')
     }
@@ -30,6 +46,7 @@ export class TrackerService {
         event.type !== TrackerService.EVENT_TYPE_CLIENT)) {
       throw new Error('invalid tracking event type')
     }
+    // TODO: add key validation
   }
 }
 TrackerService.EVENT_TYPE_WINDOW = 'window'
