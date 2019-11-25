5864a86f76cdc465a3ec6d12d42113bc31fadcbf
Nils Müller <nils.mueller@strg.at>
2019-11-25 15:18:42 +0100

    Fix: exports in index.js BHV-466

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 export { TrackerAPI } from './api/tracker.api'
 export { TrackerService } from './service/tracker.service'
-export { ClientLocalStorage } from './dao/client.local.storage'
-export { ClientSessionStorage } from './dao/client.session.storage'
-export { ClientScopeStorage } from './dao/client.scope.storage'
+export { ClientStorage } from './dao/client.storage'
 export { TrackerWS } from './dao/tracker.ws'
 // TODO: remove plugins from tracking
 export { createScrollTracking } from './plugins/plugin.scroll'
