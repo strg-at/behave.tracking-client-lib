44c2ac0f6a11d37bc4fd3900a064a1a1fdbed0a9
Nils Müller <nils.mueller@strg.at>
2019-11-26 15:16:06 +0100

    Change: removed dead code BHV-466

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ export { TrackerAPI } from './api/tracker.api'
 export { TrackerService } from './service/tracker.service'
 export { ClientStorage } from './dao/client.storage'
 export { TrackerWS } from './dao/tracker.ws'
-// TODO: remove plugins from tracking
-export { createScrollTracking } from './plugins/plugin.scroll'
