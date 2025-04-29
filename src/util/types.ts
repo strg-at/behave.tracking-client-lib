f5210695d66b0cb33e9f39da847d456e2277e9d5
lillijo <lillijoppien@gmail.com>
2025-04-29 17:15:13 +0200

    feat: add global types, RES-118

---
+++ src/util/types.ts
@@ -0,0 +1,61 @@
+export type ClientConfiguration = {
+    NAMESPACE: string
+    ENDPOINT: string
+    RECONNECT_TIMEOUT: number
+    CLIENT_STORAGE_NAMESPACE: string
+}
+
+export type StorageType = 'local' | 'session' | 'scope'
+
+export type TrackerEvent = {
+    key: string
+    value: string
+    session?: string
+    window?: string
+    client?: string
+    time?: number
+    content?: string
+}
+export type PluginEvent = {
+    key: string;
+    value: string | number | null;
+    time: number;
+    [key: string]: string | number | null
+}
+
+export type PluginEventCallback = (event: PluginEvent) => void
+
+export type EventName = 'visibility' | 'breakpoint'
+
+export type PluginEventCallbacks = {
+    [key: string]: PluginEventCallback[]
+}
+
+export type ScrollTrackingDefaults = {
+    THROTTLE_DELAY: number
+    GAUGE_POINT_INTVERVAL: number
+}
+
+export type TrackingRect = {
+    top: number
+    right: number
+    bottom: number
+    left: number
+    height: number
+}
+
+export type VisibilityEventOptions = {
+    eventKey: string
+    eventValue: number
+    visibilityThreshold: number
+}
+
+export type ScrollDepthEventOptions = {
+    eventKey: string
+    gaugePointInterval: number | null
+    id?: string
+    value?: string | number
+}
+
+
+export type ThrottleOptions = { leading?: boolean, trailing?: boolean }
