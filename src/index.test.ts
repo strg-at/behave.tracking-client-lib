5691f088c8ba7ff4989ae2cbc312b6ff0bbeef64
lillijo <lillijoppien@gmail.com>
2025-04-29 17:15:30 +0200

    feat: reqrite test to typescript, RES-118

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,12 @@
 /* eslint-env jest */
+
 import { TrackerAPI } from './api/tracker.api'
 import { TrackerService } from './service/tracker.service'
 import { TrackerWS } from './dao/tracker.ws'
 import { ClientStorage } from './dao/client.storage'
 
 const config = {
-  NAMESPACE: 'test',
+  NAMESPACE: 'testname',
   ENDPOINT: ' wss://behave.test.at/ws/event',
   RECONNECT_TIMEOUT: 60000,
   CLIENT_STORAGE_NAMESPACE: 'test',
@@ -15,45 +16,58 @@ beforeEach(() => {
   const dao = new TrackerWS(config)
   const clientStorage = new ClientStorage(config)
   const service = new TrackerService(dao, clientStorage, config)
-  new TrackerAPI(service, config) // eslint-disable-line no-new
-  // mock console.error
+  new TrackerAPI(service, config)
   console.error = jest.fn()
+  console.log = jest.fn()
 })
 
 test('Init produces global namespace, member push is typeof function', () => {
-  expect(typeof global[config.NAMESPACE].push).toBe('function')
+  expect(typeof window[config.NAMESPACE].push).toBe('function')
 })
 
 test('accept multiple objects in push', () => {
-  const result = global.test.push({ key: 'test', value: 'test' }, { key: 'test2', value: 'test2' })
+  const result = window.testname.push({ key: 'test', value: 'test' }, { key: 'test2', value: 'test2' })
   expect(console.error).toHaveBeenCalledTimes(0)
   expect(result).toBe(2)
 })
 
 test('do not accept on missing key in object', () => {
-  const result = global.test.push({ key: undefined, value: 'test' }, { value: 'test' })
+  // @ts-expect-error
+  const result = window.testname.push({ key: undefined, value: 'test' }, { value: 'test' })
   expect(console.error).toHaveBeenCalledTimes(2)
   expect(result).toBe(0)
 })
 
 test('do not accept on missing value in object', () => {
-  const result = global.test.push({ key: 'test', value: undefined }, { key: 'test' })
+  // @ts-expect-error
+  const result = window.testname.push({ key: 'test', value: undefined }, { key: 'test' })
   expect(console.error).toHaveBeenCalledTimes(2)
   expect(result).toBe(0)
 })
 
 test('accept only valid key value pairs', () => {
-  const result = global.test.push({ key: 'test', value: undefined }, { key: 'test', value: 'test' })
+  // @ts-expect-error
+  const result = window.testname.push({ key: 'test', value: undefined }, { key: 'test', value: 'test' })
   expect(console.error).toHaveBeenCalledTimes(1)
   expect(result).toBe(1)
 })
 
-test('read global namespace when initializing', () => {
-  global[config.NAMESPACE] = []
-  global.test.push({ key: 'test', value: 'test' }, { value: 'test' })
+test('read window namespace when initializing', () => {
+  // @ts-expect-error
+  window[config.NAMESPACE] = []
+  // @ts-expect-error
+  window.testname.push({ key: 'test', value: 'test' }, { value: 'test' })
   const dao = new TrackerWS(config)
   const clientStorage = new ClientStorage(config)
   const service = new TrackerService(dao, clientStorage, config)
-  new TrackerAPI(service, config) // eslint-disable-line no-new
+  new TrackerAPI(service, config)
   expect(console.error).toHaveBeenCalledTimes(1)
+})
+
+test('should attempt to reconnect when connection is closed unexpectedly', () => {
+  const dao = new TrackerWS(config)
+  dao.connection?.close()
+  expect(console.log).toHaveBeenCalledTimes(1)
+  //expect(console.log).toHaveBeenCalledWith('connection lost, try reconnect in 60000 ms')
+  jest.useRealTimers()
 })
